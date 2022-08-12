ecc8cc01d6e651ea3a8e716e85937ae54f63c54d
Greg Ford <88810845+gregsford@users.noreply.github.com>
2022-08-11 23:58:45 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-const galleryPic = document.getElementById("galleryPic");
+const images = document.querySelectorAll(".images img");
+const modal = document.querySelector(".modal");
+const modalImg = document.querySelector(".modalImg");
+const close = document.querySelector(".close");
+
+images.forEach((image) => {
+  image.addEventListener("click", () => {
+    modalImg.src = image.src;
+    modal.classList.add("appear");
+
+    close.addEventListener("click", () => {
+      modal.classList.remove("appear");
+    });
+  });
+});
