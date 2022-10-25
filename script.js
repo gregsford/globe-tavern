9afc0d1ee07837982681718a008dcde0ccc66ee1
Greg Ford <88810845+gregsford@users.noreply.github.com>
2022-10-25 19:59:17 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
-const images = document.querySelectorAll(".images img");
-const modal = document.querySelector(".modal");
-const modalImg = document.querySelector(".modalImg");
-const close = document.querySelector(".close");
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
 
-images.forEach((image) => {
-  image.addEventListener("click", () => {
-    modalImg.src = image.src;
-    modal.classList.add("appear");
-
-    close.addEventListener("click", () => {
-      modal.classList.remove("appear");
-    });
-  });
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
 });
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.querySelectorAll("#img");
+var modalImg = document.querySelectorAll("#img01");
+for (let i = 0; i < img.length; i++) {
+  img[i].onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+  }
+}
+// }
+// img.onclick = function(){
+//   modal.style.display = "block";
+//   modalImg.src = this.src;
+// }
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+
+// for (let i = 0; i < img.length; i++) {
+//   img[i].onclick = function () {
+//     modal.style.display = "block";
+//     modalImg.src = this.src;
+//   }
+// }
